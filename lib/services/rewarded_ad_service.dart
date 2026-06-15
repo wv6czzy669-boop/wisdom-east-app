55f7f689fac6def60b49031a1b7fc0fe2197003a
Dogukan Isik <senin_github_mailin>
2026-06-15 09:36:43 +0300

    Complete rewarded ad service state helpers

--- lib/services/rewarded_ad_service.dart
+++ lib/services/rewarded_ad_service.dart
@@ -14,8 +14,69 @@ class RewardedAdService {
   static const String rewardedAdUnitId =
       'ca-app-pub-1367967256658706/4388775484';
 
+  bool get canShowAd => isRewardedAdReady && rewardedAd != null;
+
+  bool get shouldShowPreparingMessage => isLoadingRewardedAd;
+
+  void markRewardEarned() {
+    adRewardEarned = true;
+  }
+
+  void beginShowSession() {
+    adRewardEarned = false;
+    adShowInProgress = true;
+    adSessionId += 1;
+  }
+
+  bool isCurrentShowSession(int sessionId) {
+    return sessionId == adSessionId;
+  }
+
+  int beginRetrySession() {
+    adRetrySessionId += 1;
+    return adRetrySessionId;
+  }
+
+  bool isCurrentRetrySession(int sessionId) {
+    return sessionId == adRetrySessionId;
+  }
+
+  void markLoading() {
+    isLoadingRewardedAd = true;
+  }
+
+  void markLoaded(RewardedAd ad) {
+    rewardedAd = ad;
+    isLoadingRewardedAd = false;
+    isRewardedAdReady = true;
+  }
+
+  void markLoadFailed() {
+    rewardedAd = null;
+    isLoadingRewardedAd = false;
+    isRewardedAdReady = false;
+  }
+
+  RewardedAd? takeAdForShowing() {
+    final adToShow = rewardedAd;
+    rewardedAd = null;
+    isRewardedAdReady = false;
+    return adToShow;
+  }
+
+  void resetRewardedAdState() {
+    adShowInProgress = false;
+    adRewardEarned = false;
+    isRewardedAdReady = false;
+    rewardedAd = null;
+  }
+
   void dispose() {
     rewardedAd?.dispose();
     rewardedAd = null;
+    isRewardedAdReady = false;
+    isLoadingRewardedAd = false;
+    adShowInProgress = false;
+    adRewardEarned = false;
   }
 }
